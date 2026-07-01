341eed776436b98ca0090353fd5c6b2476583c61
oumaima.mhaouel@gmail.com <your_emailexample.com>
2026-07-01 11:57:08 +0100

    Initial commit clean (no secrets)

---
+++ lab4_mcp2.py
@@ -0,0 +1,40 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "9c51ff64",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "import asyncio\n",
+    "from langchain.agents import create_agent\n",
+    "from langchain_mcp_adapters.client import MultiServerMCPClient\n",
+    "from langchain.agents import create_agent\n",
+    "from langchain.messages import HumanMessage\n",
+    "from langchain_ollama import ChatOllama"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "TP1",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.14.3"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
